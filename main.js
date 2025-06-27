d827520f53e20c80b38d86c7544d43f190d65e29
JavierMilan <jamilanfe@gmail.com>
2025-06-27 18:44:06 +0200

    resultado

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const resultado = document.getElementById("resultado");
  /* 
  document => todo el contenido del Html
 
- .getElementById() => buscame un elemento por si Id
+ .getElementById() => buscame un elemento por su Id
  "resultado" seria su nombre y en el html
   se encuentra en  <input type="text" id="resultado" disabled>*/
   
@@ -17,24 +17,34 @@ const botones = document.querySelectorAll("button");
 
  let operacion = "";
 
- /* variable donde se va a guardar cada acción donde pultamos un botón*/
+ /* variable donde se va a guardar cada acción donde pulsamos un botón*/
 
 botones.forEach(boton => {
    /* 
-   botones=> es una lista con todos los botones de la calculadora( <div class="botones">)
+   botones=> es una lista con todos los botones de la 
+   calculadora( <div class="botones">)
    .forEach(...) => para cada boton de esa lista, haz lo siguiente..
-   boton => {...} => "cuando estés en un boton especifico, llama a ese 
-   boton y ejecuta esto que hay entre llaves {}"*/ 
+   boton => {...} => "cuando estés en un boton especifico, llama 
+   a ese boton y ejecuta esto que hay entre llaves {}"
+   */ 
 
-  boton.addEventListener('click', () => {
-    const valor = boton.textContent;
+boton.addEventListener('click', () => {
+  const valor = boton.textContent;
 
-    /* */ 
+    /*
+   Dentro del forEach, le estamos diciendo a cada botón:
+   "Oye botón, cuando te hagan clic (addEventListener('click', ...)), quiero que 
+   ejecutes este código que tengo dentro."
+
+   Entonces, cada botón al ser clicado, va a guardar el texto del botón,
+   por ejemplo, "1", "+", "="
+   y agregarlo a la operación, 
+   calcular, borrar, etc.) */ 
 
     if (valor === 'C') {
       // Si el botón es "C", limpiamos todo
       operacion = "";
-      resultado.value = "";
+      resultado.value = "0";
     } else if (valor === '←') {
       // Si el botón es ←, borramos el último carácter
       operacion = operacion.slice(0, -1);
@@ -42,6 +52,7 @@ botones.forEach(boton => {
     } else if (valor === '=') {
       // Si el botón es "=", evaluamos la operación
       try {
+        
         // Reemplazamos la "x" por "*"
         operacion = operacion.replace(/x/g, '*');
         resultado.value = eval(operacion); // Calculamos el resultado
